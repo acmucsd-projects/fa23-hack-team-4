e958956f6c8be2550e61dca69857df2a583d67d5
Angie-n <68796619+Angie-n@users.noreply.github.com>
2023-11-01 19:51:51 -0700

    Added properties

--- server/models/offer.js
+++ server/models/offer.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const offerSchema = new Schema({
-
+    post: {type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true},
+    buyer: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    price: {type: Number, min: 0, required: true},
+    comment: {type: String, maxLength: 250},
+    timestamp: {type: Date, default: Date.now},
+    is_active: {type: Boolean, default: true}
 });
 
 const Offer = mongoose.model('Offer', offerSchema);
--- server/models/product.js
+++ server/models/product.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-
+    name: {type:String, maxLength: 70, required: true},
+    description: {type:String, maxLength: 1000, required: true},
+    price: {type: Number, min: 0, required: true},
+    image: {data: Buffer, contentType: String, required: true},
+    seller: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    is_available: {type: Boolean, default: true},
+    categories: [String],
+    offers: {offer: [Schema.Types.ObjectId], ref: 'Offer',  default: []},
 });
 
 const Product = mongoose.model('Product', productSchema);
