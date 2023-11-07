a47d3d4edd07fc47b1696d0a7380389f55610bbf
mayhong1 <maythegreat.minecraft@gmail.com>
2023-11-06 19:17:34 -0800

    User/Post Initial Commit
    Added initial code to the user and post schemas.

--- server/models/post.js
+++ server/models/post.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-
+    post_id: {type: Number, maxLength: 70, required: true},
+    title: {type: String, required: true},
+    date_created: {type: Date},
+    seller: {type: String, required: true}, 
+    product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
 });
 
 const Post = mongoose.model('Post', postSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
+    user_id: {type: Number, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    name: {type: String, required: true},
+    dateOfRegistration: {type: Date, default: Date.now}, 
+    saved_posts: [{type: Schema.Types.ObjectId, ref: 'Post', }], required: true, 
+    created_posts: [{type: Schema.Types.ObjectId, ref: 'Post', }], required: true,      
+    contact_method: {type: String, required: true}
 });
 
 const User = mongoose.model('User', userSchema);
