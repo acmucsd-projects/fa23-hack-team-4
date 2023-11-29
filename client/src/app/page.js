e331014e8ba810bbb6cb4891d2d23cbd3fa6aac9
kirustar14 <kiruthika.star14@gmail.com>
2023-11-28 19:19:10 -0800

    test

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -6,7 +6,8 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p> 
-          Hi. Get started by editing&nbsp;
+          hello...... Get started by editing&nbsp;
+          hello 2
           <code className={styles.code}>src/app/page.js</code>
         </p> 
         <div>
