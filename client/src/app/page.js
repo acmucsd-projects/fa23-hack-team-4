9e9c0cdc63e06618b3ecc3934fc36cf9fd1828b0
Samuel Eun <samuel9eun@gmail.com>
2023-12-19 13:12:14 -0800

    made dashboard page

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Logo from './triton-marketplace.png'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.logo}>
-         /* insert logo here */ 
+         <img src={Logo} />
       </div>
     </main>
   )
