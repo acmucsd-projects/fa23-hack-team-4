4c087d5648e8eabe187a41b2ac6a437e9efd7e98
Samuel Eun <samuel9eun@gmail.com>
2023-12-19 14:59:59 -0800

    attempting to add logo image

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,12 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Logo from './triton-marketplace.png'
 
 export default function Home() {
+  const logo = '/src/images/triton-marketplace.jpeg';
+
   return (
     <main className={styles.main}>
-      <div className={styles.logo}>
-         <img src={Logo} />
+      <div>
+         <Image src={logo} width={100} height={1000} alt='Triton Marketplace Free and For Sale'/>
       </div>
     </main>
   )
