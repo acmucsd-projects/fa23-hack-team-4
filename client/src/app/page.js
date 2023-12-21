cbbf9d368843fc54178c06fe316eb8484dcc28f3
Samuel Eun <samuel9eun@gmail.com>
2023-12-21 00:07:19 -0800

    completed landing page front end (non-responsive, non-integrated)

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import { inter } from 'next/font/google'
 import styles from './page.module.css'
 
 export default function Home() {
@@ -16,19 +15,23 @@ export default function Home() {
         objectFit='cover'
         objectPosition='center'
       />
-      <Header />
-      <Account />
+      <div className={styles.header}>
+        <Title />
+      </div>
+      <div className={styles.body}>
+        <Account />
+      </div>
     </main>
   )
 }
 
 
-function Header() {
+function Title() {
 
   const logo = '/images/TM-logo.png';
 
   return (
-    <div className={styles.header}>
+    <div className={styles.title}>
       <Image 
         src={logo}
         style={{
@@ -40,7 +43,7 @@ function Header() {
         alt='Triton Marketplace Free and For Sale'
         priority
       />
-      <div className={styles.title}>
+      <div className={styles.name}>
         <h1>Triton Marketplace</h1>
         <h2>UCSD Free and For Sale</h2>
       </div>
@@ -55,17 +58,23 @@ function Account() {
         <div className={styles.loginPrompt}>
           <h2>Welcome back!</h2>
           <h1>Login for the goods:</h1><br />
-          <form className={styles.loginForm}>
-            <label for='username'>Username (before @ucsd.edu)</label><br />
+          <form>
+            <label className={styles.label} for='username'>Username (before @ucsd.edu)</label><br />
             <input type='text' id='username' name='username' placeholder='Username' /><br /><br />
-            <label for='password'>Password</label><br />
+            <label className={styles.label} for='password'>Password</label><br />
             <input type='password' id='password' name='password' placeholder='Password' /><br /><br/>
             <input type='submit' value='Submit' />
           </form>
         </div>
       </div>
       <div className={styles.signUp}>
-
+        <div className={styles.signUpPrompt}>
+          <h1>New Here?</h1>
+          <h2>Join to sell goods amongst UCSD students, faculty, and alumni!</h2>
+          <form>
+            <input type='button' value='Sign Up!' />
+          </form>
+        </div>
       </div>
     </div>
   )
