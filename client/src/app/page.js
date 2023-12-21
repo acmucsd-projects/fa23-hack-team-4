65520eb20cb13b896558f0ba4d6e1625284af272
Samuel Eun <samuel9eun@gmail.com>
2023-12-21 02:42:00 -0800

    added framer motion package, restructured classes for animation and flexibility, use client (not sure how this will affect routing)

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,5 +1,8 @@
+"use client"
 import Image from 'next/image'
 import styles from './page.module.css'
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 
 export default function Home() {
 
@@ -52,30 +55,99 @@ function Title() {
 }
 
 function Account() {
+
+  const [isLogin, setLogin] = useState(true);
+
+  const toggleSwitch = () => {
+    setLogin(!isLogin)
+  }
+
+  const accountVariant = {
+    login: {
+      borderRadius: '30px 120px'
+    },
+    signUp: {
+      borderRadius: '120px 30px'
+    }
+  }
+
+  const signUpVariant = {
+    login: {
+      borderRadius: '30px 3px 3px 120px'
+    },
+    signUp: {
+      borderRadius: '3px 30px 120px 3px'
+    }
+  }
+
+  const spring = {
+    type: 'spring',
+    stiffness: 500,
+    damping: 75
+  }
+
+  function HighlightPrompt() {
+    return (
+      <div className={styles.highlightedPrompt}>
+        <h1>New Here?</h1>
+        <h2>Join to buy and sell goods amongst UCSD students, faculty, and alumni!</h2>
+        <form>
+          <motion.input 
+            type='button' 
+            value='Sign Up!'
+            onClick={toggleSwitch}
+            whileHover={{ scale: 1.04 }}
+            whileTap={{ scale: 0.96}}
+          />
+        </form>
+      </div>
+    )
+  }
+
+  function ExpandPrompt() {
+    return (
+      <div className={styles.expandedPrompt}>
+        <h2>Welcome back!</h2>
+        <h1>Login for the goods:</h1><br />
+        <form>
+          <label for='username'>Username (before @ucsd.edu)</label><br />
+          <input type='text' id='username' name='username' placeholder='Username' /><br /><br />
+          <label for='password'>Password</label><br />
+          <input type='password' id='password' name='password' placeholder='Password' /><br /><br/>
+          <motion.input 
+            type='submit' 
+            value='Submit' 
+            whileHover={{ scale: 1.04 }}
+            whileTap={{ scale: 0.96 }}
+          />
+        </form>
+      </div>
+    )
+  }
+
   return (
-    <div className={styles.account}>
-      <div className={styles.login}>
-        <div className={styles.loginPrompt}>
-          <h2>Welcome back!</h2>
-          <h1>Login for the goods:</h1><br />
-          <form>
-            <label className={styles.label} for='username'>Username (before @ucsd.edu)</label><br />
-            <input type='text' id='username' name='username' placeholder='Username' /><br /><br />
-            <label className={styles.label} for='password'>Password</label><br />
-            <input type='password' id='password' name='password' placeholder='Password' /><br /><br/>
-            <input type='submit' value='Submit' />
-          </form>
-        </div>
-      </div>
-      <div className={styles.signUp}>
-        <div className={styles.signUpPrompt}>
-          <h1>New Here?</h1>
-          <h2>Join to sell goods amongst UCSD students, faculty, and alumni!</h2>
-          <form>
-            <input type='button' value='Sign Up!' />
-          </form>
-        </div>
-      </div>
-    </div>
+    <motion.div 
+      className={styles.account} 
+      islogin={isLogin.toString()}
+      initial={'login'}
+      animate={isLogin ? 'login' : 'signUp'}
+      variants={accountVariant}
+    >
+      <motion.div 
+        className={styles.highlighted} 
+        variants={signUpVariant}
+        layout 
+        transition={spring}
+      >
+        <HighlightPrompt />
+      </motion.div>
+      <motion.div 
+        className={styles.expanded} 
+        layout 
+        transition={spring}
+      >
+        <ExpandPrompt />
+      </motion.div>
+    </motion.div>
   )
 }
