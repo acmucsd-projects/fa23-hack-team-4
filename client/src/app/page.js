62c828900838d50914cb3d1397cf24780fd89804
Samuel Eun <samuel9eun@gmail.com>
2023-12-25 00:40:07 -0800

    Completed landing page, ready for integration

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -21,9 +21,13 @@ export default function Home() {
       <div className={styles.header}>
         <Title />
       </div>
-      <div className={styles.body}>
+      <motion.div 
+        className={styles.body}
+        initial={{opacity: 0, y: 75}}
+        animate={{opacity: 1, y: 0}}
+      >
         <Account />
-      </div>
+      </motion.div>
     </main>
   )
 }
@@ -90,7 +94,29 @@ function Account() {
 
     if(isLogin) {
       return (
-        <motion.div className={styles.condensedPrompt} layout>
+        <motion.div 
+          className={styles.condensedPrompt} 
+          layout
+          initial={{ 
+            opacity: 0,
+            x: 90
+          }}
+          animate={{ 
+            opacity: 1,
+            x: 0
+          }}
+          transition={{
+            duration: 0.25,
+            delay: 0.25
+          }}
+          exit={{ 
+            transition: {
+                duration: 0.25
+            },
+            opacity: 0,
+            x: 180
+          }}
+        >
           <h1>New Here?</h1>
           <h2>Join to buy and sell goods amongst UCSD students, faculty, and alumni!</h2>
           <form>
@@ -107,21 +133,42 @@ function Account() {
     }
 
     return (
-      <motion.div className={styles.expandedPrompt} id={styles.signUpPrompt} layout>
-          <h2>Welcome!</h2>
-          <h1>Sign up and head in:</h1>
-          <h3>Join your peers with your @ucsd.edu account!</h3><br />
-          <form>
-            <label for='username'>Email (Must be a @ucsd.edu address):</label><br />
-            <input type='text' id='username' name='username' placeholder='Username' /><br /><br />
-            <motion.input 
-              type='submit' 
-              value='Submit' 
-              whileHover={{ scale: 1.04 }}
-              whileTap={{ scale: 0.96 }}
-            />
-          </form>
-        </motion.div>
+      <motion.div 
+        className={styles.expandedPrompt} 
+        layout
+        initial={{ 
+          opacity: 0,
+          x: -50
+        }}
+        animate={{ 
+          opacity: 1,
+          x: 0
+        }}
+        transition={{
+          duration: 0.25,
+          delay: 0.25
+        }}
+        exit={{ 
+          transition: {
+              duration: 0
+          },
+          opacity: 0
+        }}
+      >
+        <h2>Welcome!</h2>
+        <h1>Sign up and head in:</h1>
+        <h3>Join your peers with your @ucsd.edu account!</h3><br />
+        <form>
+          <label for='username'>Email (Must be a @ucsd.edu address):</label><br />
+          <input type='text' id='username' name='username' placeholder='Username' /><br /><br />
+          <motion.input 
+            type='submit' 
+            value='Submit' 
+            whileHover={{ scale: 1.04 }}
+            whileTap={{ scale: 0.96 }}
+          />
+        </form>
+      </motion.div>
     )
   }
 
@@ -129,7 +176,28 @@ function Account() {
 
     if(isLogin) {
       return (
-        <motion.div className={styles.expandedPrompt} layout>
+        <motion.div 
+          className={styles.expandedPrompt} 
+          layout
+          initial={{ 
+            opacity: 0,
+            x: 50
+          }}
+          animate={{ 
+            opacity: 1,
+            x: 0
+          }}
+          transition={{
+            duration: 0.25,
+            delay: 0.25
+          }}
+          exit={{ 
+            transition: {
+                duration: 0
+            },
+            opacity: 0
+          }}
+        >
           <h2>Welcome back!</h2>
           <h1>Login for the goods:</h1><br />
           <form>
@@ -149,7 +217,29 @@ function Account() {
     }
 
     return (
-      <motion.div className={styles.condensedPrompt} layout>
+      <motion.div 
+          className={styles.condensedPrompt} 
+          layout
+          initial={{ 
+            opacity: 0,
+            x: -90
+          }}
+          animate={{ 
+            opacity: 1,
+            x: 0
+          }}
+          transition={{
+            duration: 0.25,
+            delay: 0.25
+          }}
+          exit={{ 
+            transition: {
+                duration: 0.25
+            },
+            opacity: 0,
+            x: -180
+          }}
+        >
         <h1>Returning?</h1>
         <h2>Login with your username to call dibs on your favorite items!</h2>
         <form>
