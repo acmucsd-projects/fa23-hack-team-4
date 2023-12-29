0adee2bf6636b668b0c8f9fd54795c13325f07cf
pranavRedBus <pranav.reddy0108@gmail.com>
2023-12-28 16:50:07 -0800

    redoing hub-controller, slight changes to sidebar

--- server/controllers/hubController.js
+++ server/controllers/hubController.js
@@ -1,17 +1,49 @@
 const { body, validationResult } = require("express-validator");
+const Offer = require('../models/offer');
+const User = require('../models/user');
 
 exports.hub = (req, res) => {
+
     res.send('main hub page');
 }
 
 exports.offer_list = (req, res) => {
-    res.send('list of offers');
+    Offer.find({})
+    .sort({timestamp: -1})
+    .exec((err, list_offers) => {
+        if(err) return err;
+
+        res.json(list_offers);
+    });
 }
 
+//Detail for offer with id of 
 exports.offer_detail = (req, res) => {
-    res.send('Detail for offer with id of ' + req.params.id);
+    Offer.findById(req.params.id)
+    .exec((err, offer_result) => {
+        if(offer_result == null) res.status(404).res.json({ error: 'Page not found' });
+        if(err) return err;
+        res.json(offer_result);
+    });
 }
 
+//list of saved posts'
 exports.saved_post_list = (req, res) => {
-    res.send('list of saved posts');
+    User.find({categories: req.params.saved_posts})
+        .sort({date_created: -1})
+    
+    .exec((err, list_offers) => {
+        if(err) return err;
+
+        res.json(list_post);
+    });
+}
+
+//create functions
+exports.create_get = (req, res) => {
+    res.send('create offer page');
+}
+
+exports.create_offer = (req, res) => {
+    
 }
