567f92b9c0be5257adf33b7e0529199ff39d7fb1
Angie-n <68796619+Angie-n@users.noreply.github.com>
2023-12-30 00:54:15 -0800

    Removed redundant properties and changed existing

--- server/models/post.js
+++ server/models/post.js
@@ -5,7 +5,6 @@ const postSchema = new Schema({
     title: {type: String, maxLength: 80, required: true},
     description: {type:String, maxLength: 1000, required: true},
     date_created: {type: Date, default: Date.now},
-    seller: {type: mongoose.Schema.Types.ObjectId, ref:'User', required: true}, 
     product: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},
 });
 
