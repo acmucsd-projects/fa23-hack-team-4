ab94d8cfe9875a1dc25550225df446ed6d2edd59
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-01-01 19:33:20 -0800

    Fixed redirect after entering info into SSO

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.get('/google', authController.google);
+router.get('/google/success', authController.google_success);
 router.get('/google/failure', authController.google_failure);
 router.get('/google/callback', authController.google_callback);
 router.get('/logout', authController.logout);
