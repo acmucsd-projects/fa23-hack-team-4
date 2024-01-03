e5fc981e0d09183630d9b5d232cb835897eda909
pranavRedBus <pranav.reddy0108@gmail.com>
2024-01-03 12:49:40 -0800

    More updates for hub Ctrlr

--- server/controllers/hubController.js
+++ server/controllers/hubController.js
@@ -1,6 +1,7 @@
 const { body, validationResult } = require("express-validator");
 const Offer = require('../models/offer');
 const User = require('../models/user');
+const Product = require('../models/product');
 
 exports.hub = (req, res) => {
 
@@ -28,22 +29,73 @@ exports.offer_detail = (req, res) => {
 }
 
 //list of saved posts'
+//For saved_post_list, you’ll be getting the saved posts from the user that is currently logged in.
 exports.saved_post_list = (req, res) => {
-    User.find({categories: req.params.saved_posts})
+    User.find({categories: req.user.saved_posts})
         .sort({date_created: -1})
     
-    .exec((err, list_offers) => {
+    .exec((err, list_saved_posts) => {
         if(err) return err;
 
-        res.json(list_post);
+        res.json(list_saved_posts);
     });
 }
 
 //create functions
-exports.create_get = (req, res) => {
+exports.offers_create_get = (req, res) => {
     res.send('create offer page');
 }
 
-exports.create_offer = (req, res) => {
-    
-}
+exports.offers_create_post = (req, res) => {
+    body("price", "Price must be between $0.25 and Asking Price.") //25cents cuz notebooks around there
+         .trim()
+    //     function to check if price is in bounds
+         .escape(),
+    body("comment", "Comment must not be empty.")
+        .trim()
+        .isLength({ min: 1, max: 150})
+        .escape(),
+    body('buyer').trim().escape(),
+    body('seller').trim().escape(),
+    body('product').trim().escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        Promise.all([
+            User.findById(req.body.buyer)
+            .exec((err, userResult) => {
+                if(err) return err;
+
+                res.json(userResult);
+            }),
+            
+            User.findById(req.body.seller)
+            .exec((err, userResult) => {
+                if(err) return err;
+
+                res.json(userResult);
+            }),
+        
+            Product.findById(req.body.product)
+            .exec((err, productResult) => {
+                if(err) return err;
+
+                res.json(productResult);
+            })
+        ]).then(results => {
+            if(errors.isEmpty()) {
+                const newOffer = Post({
+                    price: req.body.price,
+                    comment: req.body.comment,
+                    buyer: results[0],
+                    seller: results[1],
+                    product: results[2]
+                });
+                newOffer.save((err) => {
+                    if(err) return err;
+                    res.redirect(newOffer.url);
+                });
+            }
+        });
+    }
+};
