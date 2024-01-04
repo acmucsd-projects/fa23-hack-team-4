3b219f6d99daeb4d99e833a769a84bbe8d18abd8
Samuel Eun <samuel9eun@gmail.com>
2024-01-03 23:40:03 -0800

    started account page

--- client/src/app/dashboard/account/page.js
+++ client/src/app/dashboard/account/page.js
@@ -1,22 +1,43 @@
-
+import styles from './page.module.css'
+import PostPreview from '../components/postPreview/postPreview.js'
 
 export default function Account() {
 
-    const pageView = 0; /* Another User Viewing */
-    /* const pgaeView = 1; /* Viewing your own page */
+    const pageView = 0; /* Viewing another account */
+    /* const pgaeView = 1; /* Viewing your own account */
 
-    if(pageView = 0) {
+    if(pageView == 0) {
         return (
-            <section>
-                Account page
+            <section className={styles.body}>
+                <div className={styles.accountInfo}>
+                    Username's account
+                </div>
+                <div className={styles.posts}>
+                    <h1>Active Listings:</h1>
+                    <div className={styles.livePosts}>
+                        <PostPreview />
+                        <PostPreview />
+                        <PostPreview />
+                    </div>
+                </div>
             </section>
         )
     }
 
-    if(pageView = 1) {
+    if(pageView == 1) {
         return (
-            <section>
-                
+            <section className={styles.body}>
+                <div className={styles.accountInfo}>
+                    Your account
+                </div>
+                <div className={styles.posts}>
+                    <div className={styles.livePosts}>
+
+                    </div>
+                    <div className={styles.archivedPosts}>
+
+                    </div>
+                </div>
             </section>
         )
     }
