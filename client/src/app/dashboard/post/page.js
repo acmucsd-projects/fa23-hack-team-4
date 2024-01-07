86d6c93d12af8c8cfdd18d3c705edd1f6ac04420
Samuel Eun <samuel9eun@gmail.com>
2024-01-06 21:46:00 -0800

    Revert to 795954c

--- client/src/app/dashboard/post/page.js
+++ client/src/app/dashboard/post/page.js
@@ -69,15 +69,15 @@ function Categories(){
 
 function ImageSlide(){
 
-    var images = ["/images/geisel-free-background.jpeg", "/images/black.jpeg", "/images/TM-logo.png"];
-    var [x, setx] = useState(0);
+    const images = ["/images/geisel-free-background.jpeg", "/images/black.jpeg", "/images/TM-logo.png"];
+    const [x, setx] = useState(0);
     
-    var handlePrevious = () => {
+    const handlePrevious = () => {
         // Decrease the index, and loop to the last image if at the beginning
         setx((index) => (index === 0 ? images.length - 1 : index - 1));
     };
     
-    var handleNext = () => {
+    const handleNext = () => {
         // Increase the index, and loop to the first image if at the end
         setx((index) => (index === images.length - 1 ? 0 : index + 1));
     };
