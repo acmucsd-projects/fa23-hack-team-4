9124892229b383d3ad590a98fcc9c97a895dc8e4
Samuel Eun <samuel9eun@gmail.com>
2024-01-10 01:45:41 -0800

    restructure createPost

--- client/src/app/dashboard/createPost/page.js
+++ client/src/app/dashboard/createPost/page.js
@@ -1,26 +1,31 @@
 'use client'
 import styles from './page.module.css'
 import React, { useState } from 'react'
-import PostPreview from '../components/postPreview/postPreview.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmarkSquare } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function NewPost(){
     return(
         <section className={styles.body}>
-            <a href='/dashboard/account' className={styles.button}>X</a>
+            <a href='/dashboard/account'>
+                <FontAwesomeIcon icon={faXmarkSquare} style={{height: '2.5vw', width: '2.5vw'}}/>
+            </a>
             <div className={styles.createPost}>
+                <h1 className={styles.title1}>Please fill out the following information to create a new post</h1>
                 <form>
-                    <h1 className={styles.title1}>Please fill out the following information to create a new post</h1>
-                    <h1 className={styles.title}>Title</h1>
-                    <textarea id="title" className={styles.input} name="Title"> </textarea>
-                    <h1 className={styles.title}>Sellers Description</h1>
-                    <textarea id="price" className={styles.input} name="Seller's Description"> </textarea>
-                    <h1 className={styles.title}>Price</h1>
-                    <textarea id="seller's description" className={styles.input} name="Price"> </textarea>
-                    <h1 className={styles.title}>Select up to 5 images of your product</h1>
-                    <label for="images">
-                        <input id="images" type="file"></input>
-                    </label>
+                    <label for='title'>Title</label><br />
+                    <input type='text' id='title' name='title' placeholder='What are you selling?' autoComplete='off' required/><br />
+                    <br />
+                    <label for='desc'>Description</label><br />
+                    <input type='text' id='desc' name='desc' placeholder='Describe your item here' autoComplete='off' required/><br />
+                    <br />
+                    <label for='price'>Price</label><br />
+                    <input type='text' id='price' name='price' placeholder='"Free" or a number' autoComplete='off' required/><br />
+                    <br />
+                    <label for='images'>Select up to 5 images of your product</label><br />
+                    <input id="images" type="file"></input><br />
+                    <br />
                     <div className={styles.submit}>
                         <input type="submit" className={styles.button2}></input>
                     </div>
