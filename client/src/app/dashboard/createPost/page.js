13234367a1359b8211fb2717a22c3137d7a91e45
Samuel Eun <samuel9eun@gmail.com>
2024-01-11 01:35:04 -0800

    finished restyling components

--- client/src/app/dashboard/createPost/page.js
+++ client/src/app/dashboard/createPost/page.js
@@ -8,29 +8,74 @@ import { faXmarkSquare } from '@fortawesome/free-solid-svg-icons'
 export default function NewPost(){
     return(
         <section className={styles.body}>
-            <a href='/dashboard/account'>
-                <FontAwesomeIcon icon={faXmarkSquare} style={{height: '2.5vw', width: '2.5vw'}}/>
-            </a>
             <div className={styles.createPost}>
-                <h1 className={styles.title1}>Please fill out the following information to create a new post</h1>
-                <form>
-                    <label for='title'>Title</label><br />
-                    <input type='text' id='title' name='title' placeholder='What are you selling?' autoComplete='off' required/><br />
-                    <br />
-                    <label for='desc'>Description</label><br />
-                    <input type='text' id='desc' name='desc' placeholder='Describe your item here' autoComplete='off' required/><br />
-                    <br />
-                    <label for='price'>Price</label><br />
-                    <input type='text' id='price' name='price' placeholder='"Free" or a number' autoComplete='off' required/><br />
-                    <br />
-                    <label for='images'>Select up to 5 images of your product</label><br />
-                    <input id="images" type="file"></input><br />
-                    <br />
-                    <div className={styles.submit}>
-                        <input type="submit" className={styles.button2}></input>
+                <a href='/dashboard/account'>
+                    <FontAwesomeIcon icon={faXmarkSquare} style={{height: '2.5vw', width: '2.5vw'}}/>
+                </a>
+                <h1 className={styles.title}>New Listing Information:</h1>
+                <form id='create-product' style={{display: 'flex'}}>
+                    <div className={styles.formHalf}>
+                        <label for='product-name'>
+                            Title
+                        </label>
+                        <input type='text' id='product-name' name='product-name' placeholder='What are you selling?' autoComplete='off' required/>
+                        <br />
+
+                        <label for='product-description'>
+                            Description
+                        </label>
+                        <textarea id='product-description' name='product-description' placeholder='Describe your item here' />
+                        <br />
+
+                        <label for='product-price'>
+                            Price
+                        </label>
+                        <input type='text' id='product-price' name='product-price' placeholder='"Free" or a number' autoComplete='off' required/>
+                    </div>
+                    <div className={styles.formHalf}>
+                        <label for="product-categories" name="product-categories">
+                            Select All Applicable Categories <br />
+                            <span style={{fontSize: '1.2vw'}}>(Hold Ctrl/Command to select multiple)</span>
+                        </label>
+                        <select id="product-categories" size={8} multiple>
+                            <Category />
+                        </select>
+                        <br />
+
+                        <label for='product-image[]'>
+                            Upload up to 5 images <br />
+                            <span style={{fontSize: '1.2vw'}}>(Hold Ctrl/Command in file finder to select multiple)</span>
+                        </label>
+                        <input type='file' id='product-image[]' multiple></input>
+                        <br />
+
+                        <input type="submit" />
                     </div>
                 </form>
             </div>
         </section>
+    )
+}
+
+function Category() {
+
+    const categories = 
+        ['Clothing', 'T-Shirts', 'Hoodies & Sweatshirts', 'Tops', 
+        'Pants & Shorts', 'Hats', 'Home', 'Furniture', 'Room Decor', 
+        'Kitchenware' , 'Entertainment', 'Books', 'Sporting Goods', 
+        'Games', 'Other' , 'School Supplies', 'Writing Utensils', 
+        'Notebooks', 'Textbooks', 'Lab Supplies' , 'Miscellaneous', 
+        'Drinkware', 'Backpacks & Totes', 'Other' ];
+
+    return (
+      <>
+        {categories.map(function(cat) {
+            return (
+                <option value={cat}> 
+                    &nbsp;{cat}&nbsp;
+                </option>
+            )
+        })}
+      </>
     )
 }
