4eaec8036cdaaabcd46d0567e27eb8d48a4078ab
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-01-11 17:42:27 -0800

    Allow long usernames

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {type: String, maxLength: 12, required: true, unique: true},
+    username: {type: String, maxLength: 50, required: true, unique: true},
     email: {type: String, maxLength: 254, required: true, unique: true},
     is_verified: {type: Boolean, default: false},
     password: {type: String},
